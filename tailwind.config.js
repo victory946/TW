d09cda56b8150da41417e006da7e14106392d7ec
ukaegbu victorygold <you@example.com>
2024-12-31 04:28:38 -0800

    use tailwind.css

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html", // Include all HTML files in the root
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
